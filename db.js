951baf8746e02b7c8c745290975fffe084102f74
samoooop@gmail.com <samoooop@gmail.com>
2017-04-26 16:53:48 +0700

    fix backend econnreset

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var mysql = require('mysql');
+var AsyncPolling = require('async-polling');
 
 var dbconfig = {
     host: '139.59.100.65',
@@ -8,6 +9,8 @@ var dbconfig = {
     database: 'Company',
     port: 3306,
 }
+var connection = mysql.createConnection(dbconfig);
+connection.connect();
 
 var app = express()
 app.use(express.static("."));
@@ -15,19 +18,17 @@ app.use(express.static("."));
 app.get('/json', function(req, res) {
     console.log("A request la" + req.query.ID)
         // res.send(JSON.stringify(response))
-    var connection = mysql.createConnection(dbconfig);
-    connection.connect();
     connection.query('SELECT * from employee', function(error, results, fields) {
         if (error) console.log(error)
             // console.log('The solution is: ', results);
         res.send(results)
     });
-    connection.end();
 })
 
 app.get('/index', function(req, res) {
+    console.log("requested" + req.cookies)
     var connection = mysql.createConnection(dbconfig);
-    connection.connect();
+    // connection.connect();
     var response = {
         // [1st,2nd,3rd,4th,other]
         numberOfStudent: [1, 2, 3, 4, 5],
@@ -39,9 +40,16 @@ app.get('/index', function(req, res) {
         numberOfReward: [100, 200, 300, 400, 500],
     }
     res.send(response);
-    connection.end();
+    // connection.end();
+    console.log("requested")
 })
 
 app.listen(3000, function() {
-    console.log('Example app listening on port 3000!')
+    console.log('app listening on port 3000!')
 })
+
+AsyncPolling(function(end) {
+    // Beat
+    connection.query('SELECT 1', function(error, results, fields) {});
+    end();
+}, 3000).run();
