f6b0fae5c7ed0cd829a2fcecb8063c8757034343
samoooop@gmail.com <samoooop@gmail.com>
2017-04-28 16:49:59 +0700

    remove unused code

--- db.js
+++ db.js
@@ -7,16 +7,6 @@ var async = require('async');
 var users = require('./users.js');
 var query = require('./query.js');
 
-// var dbconfig = {
-//     host: '139.59.100.65',
-//     user: 'root',
-//     password: 'password',
-//     database: 'Company',
-//     port: 3306,
-// }
-// var connection = mysql.createConnection(dbconfig);
-// connection.connect();
-
 var app = express()
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
@@ -46,27 +36,11 @@ app.all('/auth', function(req, res) {
     res.redirect('/login.html');
 });
 
-// app.use(function(req, res, next) {
-//     if (req.cookie !== undefined && req.cookie.id !== undefined) {
-//         next();
-//     }
-//     res.redirect('/login.html');
-// });
-
 app.use(express.static("."));
 
 app.all('/logout', function(req, res) {
     req.cookie = {};
     res.redirect('/login.html');
-});
-
-app.get('/json', function(req, res) {
-    console.log("A request la" + req.query.ID);
-    // res.send(JSON.stringify(response))
-    // connection.query('SELECT * from employee', function(error, results, fields) {
-    // if (error) console.log(error)
-    // console.log('The solution is: ', results);
-    res.send(results)
 });
 
 app.get('/index', function(req, res) {
@@ -94,8 +68,6 @@ app.get('/index', function(req, res) {
         console.log(response);
         res.send(response);
     });
-    // connection.end();
-    // console.log("requested")
 });
 
 app.get('/whoami', function(req, res) {
@@ -117,9 +89,3 @@ app.get('/whoami', function(req, res) {
 app.listen(3000, function() {
     console.log('app listening on port 3000!')
 });
-
-// AsyncPolling(function(end) {
-//     // Beat
-//     connection.query('SELECT 1', function(error, results, fields) {});
-//     end();
-// }, 3000).run();
