52cbc65724833e0e110c336917c1ff2330d3bbd9
5730467421-PC <cphurinut@hotmail.com>
2017-04-30 23:13:01 +0700

    รายละเอียดนิสิต doing fine ของผู้บริหาร

--- sql/GetStudentListAll.sql
+++ sql/GetStudentListAll.sql
@@ -1,19 +1,11 @@
--- select sid,first_name,last_name,sum(grade*credit)/sum(credit) as gpx,count(g.rewardid) as rewardAmount 
--- from ((student inner join semester on student.sid = semester.sem_sid) inner join sem_consist_course 
--- 		on semester.sem_sid = sem_consist_course.student_id and semester.semid = sem_consist_course.semester_id) 
--- 		inner join course on sem_consist_course.course_id = course.cid left join got_reward g on student.sid = g.studentid
--- group by sid
--- ;
-select s.sid ,s.first_name, s.last_name, Grade.GPAX as gpax , count(g.rewardid) as rewardAmount,s.entry_year as enYear,s.tid as tid,s.mid as mid
-from (student s left join got_reward g on s.sid = g.studentid), 
-					(select sum((sem_consist_course.grade*course.credit))/sum(course.credit) AS GPAX, student.sid AS Psid ,sem_consist_course.status as status
-					from ((student left join semester on student.sid = semester.sem_sid) left join sem_consist_course 
-								on semester.sem_sid = sem_consist_course.student_id and semester.semid = sem_consist_course.semester_id) 
-								left join course on sem_consist_course.course_id = course.cid
-                                where sem_consist_course.status = 'P'
-					group by student.sid) AS Grade	
-where
-		Grade.GPAX >= 2 and s.sid = Grade.Psid and s.entry_year >= ? and s.entry_year <= ?
-		and s.tid = ?
-group by sid
+-- รายละเอียดนิสิต Doing fine
+select   s.sid ,s.first_name, s.last_name , Grade.GPAX ,concat(t.first_name ,' ',t.last_name)as TeacherName
+from 	student s inner join teacher t on s.tid = t.tid 
+	left join (	select sum(sem_consist_course.grade*course.credit)/sum(course.credit) AS GPAX, student.sid AS Psid
+			from student left join semester on student.sid = semester.sem_sid
+				inner join sem_consist_course on semester.sem_sid = sem_consist_course.student_id and semester.semid = sem_consist_course.semester_id
+				inner join course on sem_consist_course.course_id = course.cid
+			where sem_consist_course.status = 'P'
+			group by student.sid) AS Grade	on s.sid = Grade.Psid
+where s.mid = ? and ( Grade.GPAX >= 2 or Grade.GPAX is null)
 ;
