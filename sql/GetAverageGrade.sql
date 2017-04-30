627eeb147fa5eeab8fca169dce36f8eaa1b6119e
5730447921-PW <bas_kung02@hotmail.com>
2017-04-30 23:45:47 +0700

    Update GetAverageGrade.sql

--- sql/GetAverageGrade.sql
+++ sql/GetAverageGrade.sql
@@ -2,5 +2,5 @@ select sum((sem_consist_course.grade * course.credit))/sum(course.credit) AS res
 from ((student inner join semester on student.sid = semester.sem_sid) inner join sem_consist_course 
 		on semester.sem_sid = sem_consist_course.student_id and semester.semid = sem_consist_course.semester_id) 
 		inner join course on sem_consist_course.course_id = course.cid
-where student.tid = ?
+where student.tid = ? and sem_consist_course.status = 'P'
 group by student.entry_year;
